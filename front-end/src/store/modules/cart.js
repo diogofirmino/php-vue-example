bd4ad74d7052e51cee71cf77d2b9fa36f2e2e0cf
Diogo Firmino <dfirminodoamaral@gmail.com>
2021-12-29 13:00:15 -0500

    fix indentation

--- front-end/src/store/modules/cart.js
+++ front-end/src/store/modules/cart.js
@@ -1,58 +1,59 @@
 import axios from 'axios';
 
 export default {
-state: {
+  state: {
     items: [],
     featuredItem: null,
     itemsSort: null,
     loading: false,
-},
-getters: {
-    items: state => state.items,
-    featuredItem: state => state.items.find(item => item.uuid === state.featuredItem),
-    activeSorting: state => state.itemsSort,
-    isLoading: state => state.loading,
-},
-mutations: {
+  },
+  getters: {
+    items: (state) => state.items,
+    featuredItem: (state) =>
+      state.items.find((item) => item.uuid === state.featuredItem),
+    activeSorting: (state) => state.itemsSort,
+    isLoading: (state) => state.loading,
+  },
+  mutations: {
     LOAD_ITEMS(state, { items, sort }) {
-        state.items = items;
-        state.itemsSort = sort;
+      state.items = items;
+      state.itemsSort = sort;
     },
     OPEN_MODAL(state, itemUuid) {
-        state.featuredItem = itemUuid;
+      state.featuredItem = itemUuid;
     },
     CLOSE_MODAL(state) {
-        state.featuredItem = null;
+      state.featuredItem = null;
     },
     SET_LOADING(state) {
-        state.loading = true;
+      state.loading = true;
     },
     UNSET_LOADING(state) {
-        state.loading = false;
+      state.loading = false;
     },
-},
-actions: {
-    async loadItems({ commit }, sortByPrice = 'ASC') {
-        commit('SET_LOADING');
+  },
+  actions: {
+    async loadItems({ commit }, sortByPrice = "ASC") {
+      commit("SET_LOADING");
 
-        try {
-            const { data } = await axios.get(`/api/cart?sort=${sortByPrice}`);
+      try {
+        const { data } = await axios.get(`/api/cart?sort=${sortByPrice}`);
+        
+        commit("LOAD_ITEMS", { items: data.cart, sort: data.meta.sort });
+      } catch (e) {
+        console.log(e);
+      }
 
-            commit('LOAD_ITEMS', { items: data.cart, sort: data.meta.sort });
-        } catch (e) {
-            console.log(e);
-        }
-
-        commit('UNSET_LOADING');
+      commit("UNSET_LOADING");
     },
     openDetailsModal({ commit }, itemUuid) {
-        commit('OPEN_MODAL', itemUuid);
+      commit("OPEN_MODAL", itemUuid);
     },
     closeDetailsModal({ commit }) {
-        commit('CLOSE_MODAL');
+      commit("CLOSE_MODAL");
     },
     sortByPrice({ commit }, direction) {
-        commit('ORDER_ITEMS_BY_PRICE', direction);
+      commit("ORDER_ITEMS_BY_PRICE", direction);
     },
-},
-}
+  },
+};
